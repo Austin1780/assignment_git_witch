4df9f32f8b54d5aac51dcdfe7685d1b07f4e704e
Austin1780 <austinwilliamsmith@gmail.com>
2017-11-28 10:06:26 -0700

    working on first spec

--- src/GitWitch.js
+++ src/GitWitch.js
@@ -1,7 +1,14 @@
-const questionParser = require('./questionParser');
+const questionParser = require("./questionParser");
 class GitWitch {
-  constructor(string) {
-    let parsedQuestion = questionParser(string);
+  constructor(parser, runner, formatter) {
+    this.parser = parser;
+    this.runner = runner;
+    this.formatter = formatter;
+  }
+  process(string) {
+    let command = this.parser.parse(string);
+    let response = this.runner.run(command);
+    return this.formatter.format(response);
   }
 }
 
