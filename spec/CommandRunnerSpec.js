08cfe33149717b1765a5d7c153eff20927bf23cd
Austin1780 <austinwilliamsmith@gmail.com>
2017-11-28 15:01:11 -0700

    working on Command Runner spec

--- spec/CommandRunnerSpec.js
+++ spec/CommandRunnerSpec.js
@@ -4,32 +4,47 @@ describe("CommandRunner", () => {
   beforeEach(() => {
     this.runner = new CommandRunner();
   });
-  it("returns user's repos with descriptions", () => {
-    let object = {username: "griselda", subject: "repos", query: "details"};
-    const result= this.runner.run(object);
+  xit("returns user's repos with descriptions", () => {
+    let object = { username: "griselda", subject: "repos", query: "details" };
+    const result = this.runner.run(object);
     object.results = [
       { name: "repo1", description: "description of repo1", star: true },
       { name: "repo2", description: "description of repo2", star: false }
     ];
     expect(result).toEqual(object);
   });
-  it("returns user's starred repos with descriptions", () => {
-    let object = {username: "griselda", subject: "starred repos", query: "details"};
-    const result= this.runner.run(object);
-    object.results = [
-      { name: "repo1", description: "description of repo1", star: true },
-    ];
-    expect(result).toEqual(object);
+  it("returns user's starred repos with descriptions", done => {
+    let object = {
+      username: "griselda",
+      subject: "starred repos",
+      query: "details"
+    };
+    let resultsObject = Object.assign({}, object, {
+      results: [
+        { name: "repo1", description: "description of repo1", star: true }
+      ]
+    });
+    var x = this.runner.run(object);
+    this.runner.run(object).then(output => {
+      console.log(output);
+      console.log(resultsObject);
+      expect(output).toEqual(resultsObject);
+      done();
+    });
   });
-  it("returns user's repo count", () => {
-    let object = {username: "griselda", subject: "repos", query: "count"};
-    const result= this.runner.run(object);
+  xit("returns user's repo count", () => {
+    let object = { username: "griselda", subject: "repos", query: "count" };
+    const result = this.runner.run(object);
     object.results = 2;
     expect(result).toEqual(object);
   });
-  it("returns user's star count", () => {
-    let object = {username: "griselda", subject: "starred repos", query: "count"};
-    const result= this.runner.run(object);
+  xit("returns user's star count", () => {
+    let object = {
+      username: "griselda",
+      subject: "starred repos",
+      query: "count"
+    };
+    const result = this.runner.run(object);
     object.results = 1;
     expect(result).toEqual(object);
   });
--- src/CommandRunner.js
+++ src/CommandRunner.js
@@ -1,32 +1,31 @@
-let gitHub = require('./GitHub.getRepos');
+let gitHub = require("./GitHub.getRepos");
 class CommandRunner {
   constructor() {}
   run(object) {
-    return new Promise((resolve, reject) => {
-      let resultObject = object;
-      let results;
-      let resultVar = gitHub(object.username);
-      if (object.subject === 'repos') {
-        if (object.query === 'details') {
-          results = resultVar;
-        } else if (object.query === 'count') {
-          results = resultVar.length;
-        }
-      } else if (object.subject === 'starred repos') {
-        let starArray = resultVar.map(el => {
-          if (el.star) {
-            return el;
-          }
-        });
-
-        if (object.query === 'details') {
-          return starArray;
-        } else if (object.query === 'count') {
-          return starArray.length;
-        }
+    let resultObject = object;
+    let results;
+    let resultVar = gitHub(object.username);
+    if (object.subject === "repos") {
+      if (object.query === "details") {
+        results = resultVar;
+      } else if (object.query === "count") {
+        results = resultVar.length;
       }
-
-      resultObject.results = results;
+    } else if (object.subject === "starred repos") {
+      let starArray = resultVar.map(el => {
+        if (el.star) {
+          return el;
+        }
+      });
+      console.log("STAR ARRAY=" + starArray);
+      if (object.query === "details") {
+        results = starArray;
+      } else if (object.query === "count") {
+        results = starArray.length;
+      }
+    }
+    resultObject.results = results;
+    return new Promise((resolve, reject) => {
       resolve(resultObject);
     });
   }
